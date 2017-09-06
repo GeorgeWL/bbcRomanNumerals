cd743899ed40ecb54261db0074e60be0612aa7a6
George W Langham(Laptop) <georgelangham1990@gmail.com>
2017-09-06 18:12:35 +0100

    added notes, using object method instead
    split the entered number into the thousands, hundreds, tens and units instead.
    next is to create a function which takes these results and converts.

--- source.js
+++ source.js
@@ -1,13 +1,6 @@
 // Start with some Pseudo code
-// IDEA: Variable that takes the number(n)
 // IDEA: Function which takes input of n and then converts it to roman numerals.
-// IDEA: If divisible by 10 equally, repeat X for each divisible
-// IDEA: If divisible by 5 equally, repeat V for each divisible
-// IDEA: For each remainder print I
-// IDEA: for every 5 tens repeat L instead
-// IDEA: for every 10 tens repeat C instead
-// IDEA: For every fifty tens, repeat D instead
-// IDEA: for every 100 tens, repeat M instead
+// NOTE: Here's the roman numerals
 // I = 1
 // V = 5
 // X = 10
@@ -15,9 +8,9 @@
 // C = 100
 // D = 500
 // M = 1000
-// I use the JSX standard styleguide for Javascript, which argues that line endings (;) are bad practice except in certain exempt circumstances and at the end of the function. https://standardjs.com/rules.html
-// IDEA: Split into array of thousands, hundreds, tens and units. for each thousand echo a M, etc. etc.
-(function () {
+// NOTE: I use the JSX standard styleguide for Javascript, which argues that line endings () are bad practice except in certain exempt circumstances and at the end of the function. https://standardjs.com/rules.html
+// IDEA: Split into object of thousands, hundreds, tens and units. for each thousand echo a M, D, etc. etc.
+(function() {
   let number = document.querySelector('input')
   let btn = document.querySelector('button')
   // let numeral = document.querySelector('#numeral')
@@ -26,18 +19,45 @@
     // let numberTo = number.value
   })
 })()
-var number = 3999
-var quotient = 0
-var quotient1 = 0
-var quotient2 = 0
-var remainder = 0
-var remainder1 = 0
-function numerals (number) {
-  // Begin with a test case of hard-coded 3999
-  quotient = number / 1000 // This will give you 3
-  remainder = number % 1000 // This will give you 999
-  quotient1 = remainder / 100 // This will give you 9
-  remainder1 = remainder % 100 // This will give you 90
-  quotient2 = remainder1 / 10 // This will give you 9
-  document.getElementById('numeral').textContent=numeral
+
+function getMultiplesOfTen(number) {
+  document.getElementById('result').innerHTML = ''
+  number = parseInt(number)
+  if (typeof(number) !== 'number') return number
+  var result = {}
+  (function breakDown(num) {
+      if (isNaN(num)) return num // if it's invalid return
+      if (num <= 0) return false
+      num = num.toFixed(0) // get rid of decimals
+      var divisor = Math.pow(10, num.length - 1), // ex. when num = 300, divisor = 100
+        quotient = Math.floor(num / divisor)
+      result[divisor] = quotient // add it to our object
+      breakDown(num % divisor) // break down the remainder
+    })(number)
+  // return result as an object
+  return result
+}
+
+// functions for visual only, will be replaced by the result of numerals fucntion
+var input = document.getElementById('input')
+var result = document.getElementById('result')
+
+input.onkeyup = function results() {
+  var obj = getMultiplesOfTen(input.value)
+  for (var prop in obj) {
+    var table = document.getElementById('result'),
+      tr = document.createElement('tr'),
+      th = document.createElement('th'),
+      td = document.createElement('td')
+    // add data
+    td.innerHTML = obj[prop] + ' multiples of '
+    th.innerHTML = prop
+
+    // add nodes to table
+    tr.appendChild(td)
+    tr.appendChild(th)
+
+
+    table.appendChild(tr)
+  }
 }
