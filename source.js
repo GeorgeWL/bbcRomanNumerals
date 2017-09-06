908f6db4dcaa10eb0e3bab8f198804c412eef41b
George W Langham(Laptop) <georgelangham1990@gmail.com>
2017-09-06 14:41:46 +0100

    init
    First commented Ziffer of the JS, eslintrc for the options of linter.
    Package json for npm modules.

---
+++ source.js
@@ -0,0 +1,18 @@
+// Start with some Pseudo code
+// IDEA: Variable that takes the number(n)
+// IDEA: Function which takes input of n and then converts it to roman numerals.
+// IDEA: If divisible by 10 equally, repeat X for each divisible
+// IDEA: If divisible by 5 equally, repeat V for each divisible
+// IDEA: For each remainder print I
+// IDEA: for every 5 tens repeat L instead
+// IDEA: for every 10 tens repeat C instead
+// IDEA: For every fifty tens, repeat D instead
+// IDEA: for every 100 tens, repeat M instead
+int I = 1
+int V = 5
+int X = 10
+int L = 50
+int C = 100
+int D = 500
+int M = 1000
+// I use the JSX standard styleguide for Javascript, which argues that line endings (;) are bad practice except in certain exempt circumstances and at the end of the file. https://standardjs.com/rules.html
