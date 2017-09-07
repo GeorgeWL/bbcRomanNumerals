d52ec8f78493954a7faade08b6ae10b4f0b8c1e2
George W Langham(Laptop) <georgelangham1990@gmail.com>
2017-09-07 12:12:09 +0100

    Get ready for roman numerals function left a TODO for function, considering the method on paper now with pseudocode.

--- source.js
+++ source.js
@@ -10,48 +10,49 @@
 // M = 1000
 // NOTE: I use the JSX standard styleguide for Javascript, which argues that line endings () are bad practice except in certain exempt circumstances and at the end of the function. https://standardjs.com/rules.html
 // IDEA: Split into object of thousands, hundreds, tens and units. for each thousand echo a M, D, etc. etc.
-numeral = "M X C"
-function getMultiplesOfTen(number) {
-  document.getElementById('result').innerHTML = "";
-  number = parseInt(number);
-  if (typeof(number) !== "number") return number;
-  var result = {};
+// IDEA:
+numeral = 'MXC'
+// testing numeral output form web form with hard-coded output
+function getMultiplesOfTen (number) {
+  document.getElementById('result').innerHTML = ''
+  number = parseInt(number)
+  if (typeof (number) !== 'number') return number
+  var result = {}
 
-  (function breakDown(num) {
-    if (isNaN(num)) return num; //if it's invalid return
-    if (num <= 0) return false;
-    num = num.toFixed(0); //get rid of decimals
+  (function breakDown (num) {
+    if (isNaN(num)) return num // if it's invalid return
+    if (num <= 0) return false
+    num = num.toFixed(0) // get rid of decimals
 
-    var divisor = Math.pow(10, num.length - 1), //ex. when num = 300, divisor = 100
-      quotient = Math.floor(num / divisor);
-    result[divisor] = quotient; //add it to our object
-    breakDown(num % divisor); //break down the remainder
-  })(number);
-  //return result as an object
-  return result;
+    var divisor = Math.pow(10, num.length - 1), // ex. when num = 300, divisor = 100
+      quotient = Math.floor(num / divisor)
+    result[divisor] = quotient // add it to our object
+    breakDown(num % divisor) // break down the remainder
+  })(number)
+  // return result as an object
+  return result
 }
 
-//helper functions for visual only
-var input = document.getElementById('input');
-var result = document.getElementById('result');
+// functions for visuals only
+var input = document.getElementById('input')
+var result = document.getElementById('result')
 
-input.onkeyup = function results() {
-  var obj = getMultiplesOfTen(input.value);
+input.onkeyup = function results () {
+  var obj = getMultiplesOfTen(input.value)
   for (var prop in obj) {
     var table = document.getElementById('result'),
       tr = document.createElement('tr'),
       th = document.createElement('th'),
-      td = document.createElement('td');
-    //add data
-    td.innerHTML = obj[prop] + " multiples of ";
-    th.innerHTML = prop;
+      td = document.createElement('td')
+    // add data
+    td.innerHTML = obj[prop] + ' multiples of '
+    th.innerHTML = prop
 
-    //add nodes to table
-    tr.appendChild(td);
-    tr.appendChild(th);
+    // add nodes to table
+    tr.appendChild(td)
+    tr.appendChild(th)
 
-
-    table.appendChild(tr);
+    table.appendChild(tr)
   }
-
 }
+// TODO: Create a function which takes the output of Result from breakDown and then converts it to Roman Numerals
